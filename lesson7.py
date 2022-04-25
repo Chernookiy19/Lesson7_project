7f3078bd42a1e602f098b1696af4643343a148ef
Chernookiy Evgeniy <ChernookiyEvgeniy@gmail.com>
2022-04-25 21:20:41 +0300

    Added lesson7.py

---
+++ lesson7.py
@@ -0,0 +1,15 @@
+# Задача №1
+
+# def function(list: list) -> dict:
+#     dict = {}
+#     for i in list:
+#         if dict.get(i, 0):
+#             dict[i] += 1 
+#         else:
+#             dict[i] = 1
+#     return dict
+
+# numbers_list = [1, 2, 3, 4, 5, 4, 3, 2, 1]
+# print(function(numbers_list))
+
+
